431a86825cb47b5baaea0f7d33da4ec61cbd0a55
Ibrahim Mohamed <ibrah.salama@outlook.com>
2022-02-04 20:25:54 +0200

    Adding JokeService snd JokeServiceImpl. closes #4.

--- src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -2,7 +2,7 @@ package guru.springframework.spring5jokesappv2.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
-
+//Adding servcieImpl
 @Service
 public class JokeServiceImpl implements JokeService{
     private final ChuckNorrisQuotes chuckNorrisQuotes;
